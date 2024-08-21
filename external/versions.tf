a75ef7865a9a1d67298eeb222bd53310d2b9b947
lou <lou@pop-os.localdomain>
2024-08-21 12:53:46 -0400

    configure

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "gopherlou-lab""
 
     workspaces {
       name = "homelab-external"
--- platform/gitea/files/config/go.mod
+++ platform/gitea/files/config/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/gitea/config
+module git.gopherlab.win/khuedoan/homelab/gitea/config
 
 go 1.19
 
